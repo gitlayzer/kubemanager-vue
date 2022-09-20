34c5483293012c8a8b17d5552cd03a8c02e812f6
Your Name <you@example.com>
2022-09-20 14:03:33 +0800

    create kubemanager-vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- 路由占位符，会导入匹配到的$route.path的视图组件 -->
+    <router-view></router-view>
+</template>
+
+<style>
+    /*设置html和body*/
+    html, body {
+        width: 100%;
+        height: 100%;
+        padding: 0;
+        margin: 0;
+    }
+    #nprogress .bar {
+        /*自定义进度条颜色*/
+        background: #2186c0 !important;
+    }
+</style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    devServer:{
+        host: '0.0.0.0',
+        port: 80, // 启动端口号
+        open: false  // 启动后是否自动打开网页
+    }
+}
